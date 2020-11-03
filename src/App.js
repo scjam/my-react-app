41676a8016f4c5bb98fa92bb75252990ad51294a
Sydney James <sydneyjames7@gmail.com>
2020-11-03 09:55:00 -0800

    app js good to go

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import fetch from 'superagent';
+import MovieRender from './MovieRender.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  
+  state = {
+    movieData: []
+  }
+  
+  componentDidMount = async () => {
+    const response = await fetch.get(`https://warm-brushlands-73236.herokuapp.com/movies`);
+    
+    await this.setState({ movieData: response.body });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.movieData.map(movie =>
+            { return (
+              <MovieRender 
+              uniqueId={movie.id}
+              movieName={movie.name}
+              yearReleased={movie.year_released}
+              bestPicture={movie.best_picture_winner}
+              director={movie.director}
+              ownerId={movie.owner_id}
+              />
+            )
+          })
+        }
+      </div>
+    )
+  }
+  
 }
-
-export default App;
