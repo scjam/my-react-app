e1af63fa9845763562e82139f3abafd299f95282
Sydney James <sydneyjames7@gmail.com>
2020-11-03 10:18:38 -0800

    fixed boolean

--- src/MovieRender.js
+++ src/MovieRender.js
@@ -6,7 +6,7 @@ export default class MovieRender extends React.Component {
             <div className="item">
                 <p>Name: {this.props.movieName}</p>
                 <p>Year Released: {this.props.yearReleased}</p>
-                <p>Best Picture Winner?: {this.props.bestPicture.toString()}</p>
+                <p>Best Picture Winner?: {this.props.bestPicture ? 'Yes' : 'No'}</p>
                 <p>Director: {this.props.director}</p>
             </div>
         )
