e3fbc63aa923ca0862e06ad45e7db49d8c83153c
Sydney James <sydneyjames7@gmail.com>
2020-11-03 10:02:07 -0800

    render set up

---
+++ src/MovieRender.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class MovieRender extends React.Component {
+    render() {
+        return (
+            <div className="item">
+                <p>Name: {this.props.movieName}</p>
+                <p>Year Released: {this.props.yearReleased}</p>
+                <p>Best Picture Winner?: {this.props.bestPicture}</p>
+                <p>Director: {this.props.director}</p>
+            </div>
+        )
+    }
+}
