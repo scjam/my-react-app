22a31ef0fc00641afa9484ca7009c122a1f7e6d4
Sydney James <sydneyjames7@gmail.com>
2020-11-05 17:20:13 -0800

    update create page with fetch

--- src/Create.js
+++ src/Create.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import request from 'superagent';
+import { fetchDirectors, createMovie } from './Utils.js';
 
 const localStorageUser = {
     userId: 1
@@ -11,27 +11,21 @@ export default class Create extends Component {
     }
 
     componentDidMount = async () => {
-        const response = await request.get('https://warm-brushlands-73236.herokuapp.com/directors');
+        const directors = await fetchDirectors();
 
-        this.setState({ directors: response.body });
+        this.setState({ directors });
     }
 
     handleSubmit = async (e) => {
         e.preventDefault();
         
-        const newMovie = {
+        await createMovie({
             name: this.state.name,
             year_released: this.state.yearReleased,
             best_picture_winner: this.state.bestPictureWinner,
             director_id: this.state.directorId,
             owner_id: localStorageUser.userId
-        };
-        console.log(newMovie);
-        
-        await request
-            .post('https://warm-brushlands-73236.herokuapp.com/movies')
-            .send(newMovie);
-
+        });
         this.props.history.push('/');
     }
 
